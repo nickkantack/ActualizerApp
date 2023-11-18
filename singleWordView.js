69f6d72510968d7937ecae8c1ca09493d2c9cece
Nick Kantack <kantackn@amazon.com>
2023-11-18 09:02:14 -0500

    Changes to sentences table to make selection-based word adding easier

--- singleWordView.js
+++ singleWordView.js
@@ -9,23 +9,41 @@ function addSentence(sentence) {
     const cell = newRow.insertCell(0);
     cell.appendChild(textArea);
 
-    textArea.addEventListener("focus", () => {
-        textArea.classList.remove("unfocusedTextArea");
-        textArea.classList.add("focusedTextArea");
-    });
+    const editButton = document.createElement("button");
+
     textArea.addEventListener("focusout", () => {
         if (textArea.value === "") {
-            textArea.parentNode.remove(textArea);
+            newRow.remove(newRow.querySelector("tr"));
         } else {
             textArea.classList.remove("focusedTextArea");
             textArea.classList.add("unfocusedTextArea");
+            textArea.disabled = true;
+            editButton.style.display = "block";
         }
         saveCurrentSentences();
     });
 
+    const rightCell = newRow.insertCell(1);
+    rightCell.style.width = "1%";
+    editButton.classList.add("editSentenceButton");
+    editButton.innerHTML = "edit";
+    editButton.addEventListener("click", () => {
+        textArea.classList.remove("unfocusedTextArea");
+        textArea.classList.add("focusedTextArea");
+        textArea.disabled = false;
+        textArea.focus();
+        editButton.style.display = "none";
+    });
+    rightCell.appendChild(editButton);
+
+    // TODO plan to add another cell that is small and has an edit button. The textarea will remain 
+    // disabled unless this button is pressed and the button will toggle to a save, but we will still
+    // save the sentence on focus out and on back button press.
+
     if (sentence) {
         textArea.value = sentence;
         textArea.classList.add("unfocusedTextArea");
+        textArea.disabled = true;
     } else {
         textArea.focus();
     }
